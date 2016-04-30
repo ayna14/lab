3d05197edcec47c5cc7b30fbdfd3976cb81cce2f
ayna14 <aynashka14@mail.ru>
2016-04-30 11:43:00 -1200

    mvc
    mvc

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static string f = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", "file.txt");//путь до файла
+
+        public ActionResult Index(string T)//action
+        {
+            if (!string.IsNullOrWhiteSpace(T))
+            {
+                System.IO.File.AppendAllText(f, $"{T}{Environment.NewLine}");
+            }
+
+            return View();
+        }
+  
+
+        public ActionResult About()
+        {
+            ViewBag.Lines = System.IO.File.ReadAllLines(f);
+
+            return View();
+        }
+
+    }
+}
